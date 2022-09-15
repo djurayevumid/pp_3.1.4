832d7057f0a5caf96d21115106be19442cec64c0
djurayevumid <djurayevumid06@gmail.com>
2022-09-15 13:19:14 +0300

    qweqwe

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -218,3 +218,26 @@ function editUser() {
         })
     })
 }
+
+const triggerAllUsersTabList = document.querySelectorAll('#nav-tab > #nav-users-tab')
+triggerAllUsersTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', event => {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
+
+const triggerNewUserTabList = document.querySelectorAll('#nav-tab > #new-user-tab')
+triggerNewUserTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', event => {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
+
+
+
